fefafef87777ffc96f1cfbb94de19ec434c2f9ab
VinhNguyen2003 <vinhnguyenlethanh@gmail.com>
2024-12-03 15:33:15 -0600

    better code for keypad, less finnicky

--- reader-board.X/keypad.c
+++ reader-board.X/keypad.c
@@ -14,7 +14,8 @@
 #define Keypad_C1			A, 3
 #define Keypad_C2			A, 2
 
-void keyPadInit() {
+void keyPadInit() 
+{
     // Configure rows as inputs with pull-up resistors
     PORTD.DIR &= ~PIN5_bm;                // Set Keypad_R0 (D5) as input
     PORTD.PIN5CTRL |= PORT_PULLUPEN_bm;  // Enable pull-up resistor on Keypad_R0 (D5)
@@ -25,39 +26,74 @@ void keyPadInit() {
     PORTC.DIR &= ~PIN1_bm;                // Set Keypad_R3 (C1) as input
     PORTC.PIN1CTRL |= PORT_PULLUPEN_bm;  // Enable pull-up resistor on Keypad_R3 (C1)
     // Configure columns as outputs and set to low
-    PORTC.DIR |= PIN0_bm;                 // Set Keypad_C0 (C0) as output
-    PORTC.OUT &= ~PIN0_bm;                // Set Keypad_C0 (C0) to low
-    PORTA.DIR |= PIN3_bm;                 // Set Keypad_C1 (A3) as output
-    PORTA.OUT &= ~PIN3_bm;                // Set Keypad_C1 (A3) to low
-    PORTA.DIR |= PIN2_bm;                 // Set Keypad_C2 (A2) as output
-    PORTA.OUT &= ~PIN2_bm;                // Set Keypad_C2 (A2) to low
+    PORTC.DIR &= ~PIN0_bm;                 // Set Keypad_C0 (C0) as input
+    PORTA.DIR &= ~PIN3_bm;                 // Set Keypad_C1 (A3) as input
+    PORTA.DIR &= ~PIN2_bm;                 // Set Keypad_C2 (A2) as input
 }
 
 // Get the currently pressed key
-enum KeypadKey_t getKey(){
-    // Deactivate all columns before starting (set all low)
-    PORTC.OUT &= ~PIN0_bm; // Keypad_C0 low
-    PORTA.OUT &= ~PIN3_bm; // Keypad_C1 low
-    PORTA.OUT &= ~PIN2_bm; // Keypad_C2 low
+enum KeypadKey_t getKey()
+{
+    unsigned char row , col;	
     
     for (uint8_t col = 0; col < 3; col++) {
-        // Activate the current column
+        _delay_ms(10);									// wait 10ms for button de-bouncing
+        PORTC.DIR &= ~(1 << PIN0_bp);  // Set Keypad_C0 (C0) as input
+        PORTA.DIR &= ~(1 << PIN3_bp);  // Set Keypad_C1 (A3) as input
+        PORTA.DIR &= ~(1 << PIN2_bp);  // Set Keypad_C2 (A2) as input
+		// make only current column as output
         switch (col) {
-            case 0: PORTC.OUT |= PIN0_bm; break; // Set Keypad_C0 high
-            case 1: PORTA.OUT |= PIN3_bm; break; // Set Keypad_C1 high
-            case 2: PORTA.OUT |= PIN2_bm; break; // Set Keypad_C2 high
+            case 0:
+                PORTC.DIR |= (1 << PIN0_bp);  // Set Keypad_C0 as output
+                PORTC.OUT &= ~(1 << PIN0_bp); // Set Keypad_C0 to low
+                break;
+            case 1:
+                PORTA.DIR |= (1 << PIN3_bp);  // Set Keypad_C1 as output
+                PORTA.OUT &= ~(1 << PIN3_bp); // Set Keypad_C1 to low
+                break;
+            case 2:
+                PORTA.DIR |= (1 << PIN2_bp);  // Set Keypad_C2 as output
+                PORTA.OUT &= ~(1 << PIN2_bp); // Set Keypad_C2 to low
+                break;
         }
-        // Check each row for a pressed key
-        if (!(PORTD.IN & PIN5_bm)) { return kpMap[0][col]; break;} // Keypad_R0
-        if (!(PORTD.IN & PIN1_bm)) { return kpMap[1][col]; break;} // Keypad_R1
-        if (!(PORTD.IN & PIN6_bm)) { return kpMap[2][col]; break;} // Keypad_R2
-        if (!(PORTC.IN & PIN1_bm)) { return kpMap[3][col]; break;} // Keypad_R3
-        // Deactivate the current column
+		// make only current column output is low
+		for (row = 0; row < 4; row++) {   
+            // Check if the current row reads low, indicating a key press
+            switch (row) {
+                case 0:
+                    if (!(PORTD.IN & (1 << PIN5_bp))) { // Check Keypad_R0 (D5)
+                        return kpMap[row][col];
+                    }
+                    break;
+                case 1:
+                    if (!(PORTD.IN & (1 << PIN1_bp))) { // Check Keypad_R1 (D1)
+                        return kpMap[row][col];
+                    }
+                    break;
+                case 2:
+                    if (!(PORTD.IN & (1 << PIN6_bp))) { // Check Keypad_R2 (D6)
+                        return kpMap[row][col];
+                    }
+                    break;
+                case 3:
+                    if (!(PORTC.IN & (1 << PIN1_bp))) { // Check Keypad_R3 (C1)
+                        return kpMap[row][col];
+                    }
+                    break;
+            }
+        }
+        // Reset the current column
         switch (col) {
-            case 0: PORTC.OUT &= ~PIN0_bm; break; // Set Keypad_C0 low
-            case 1: PORTA.OUT &= ~PIN3_bm; break; // Set Keypad_C1 low
-            case 2: PORTA.OUT &= ~PIN2_bm; break; // Set Keypad_C2 low
+            case 0:
+                PORTC.DIR &= ~(1 << PIN0_bp); // Set Keypad_C0 to input
+                break;
+            case 1:
+                PORTA.DIR &= ~(1 << PIN3_bp); // Set Keypad_C1 to input
+                break;
+            case 2:
+                PORTA.DIR &= ~(1 << PIN2_bp); // Set Keypad_C2 to input
+                break;
         }
     }
-    return Key_None;
+    return Key_None; // No key was pressed
 }
