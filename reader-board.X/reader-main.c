84fadf8d889faf0f56621400212a7e9c5ade3cd0
VinhNguyen2003 <vinhnguyenlethanh@gmail.com>
2024-12-05 03:40:51 -0600

    state machine skeleton

--- reader-board.X/reader-main.c
+++ reader-board.X/reader-main.c
@@ -7,15 +7,49 @@
 #include "keypad.h"
 #include "bluetooth_reader.h"
 #include "rfid-reader.h"
+#include "lcd.h"
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-
+#include <string.h>
+#include <stdio.h>
 #include "../common.X/globals.h"
 #include <util/delay.h>
 
-// Setups for the keypad
+#define RFID_READER_BUFFER_SIZE 32
+#define BUF_SIZE 32
+
+// Define states
+typedef enum {
+    IDLE,
+    RFIDERR,
+    PASSCODE,
+    PASSCODEERR,
+    ACCESSGRANTED
+} LockState_t;
+
+// Global variables
+volatile LockState_t currentState = IDLE;
+LiquidCrystalDevice_t device;
 volatile uint16_t adc_result = 0;
+
+void idleState();
+void rfidErrorState();
+void passcodeState();
+void passcodeErrorState();
+void accessGrantedState();
+
+void setup() {
+    initializeUsart();
+    initializeTwi();
+    initializeSpi();
+    initializeReader();
+    bluetoothInit();
+    //bluetoothInitReader();
+    ADCKeyPadInit();
+    device = lq_init(0x27, 20, 4, LCD_5x8DOTS);
+    lq_turnOnBacklight(&device);
+}
 
 ISR(ADC0_RESRDY_vect) {
     adc_result = ADC0.RES;
@@ -33,27 +67,83 @@ ISR(ADC0_RESRDY_vect) {
 
 int main(void) {
     //    initializeCommonBoardFunctions("DoorLockReader");
-    initializeUsart();
-    initializeTwi();
-    initializeSpi();
-    initializeReader();
-    bluetoothInit();
-    ADCKeyPadInit();
+    setup();
+    sei();
+//    while (1) {
+//        bluetoothWriteBytes("TEST", 2);
+//    }
+     while (1) {
+         switch (currentState) {
+             case IDLE:
+                 idleState();
+                 break;
+             case RFIDERR:
+                 rfidErrorState();
+                 break;
+             case PASSCODE:
+                 passcodeState();
+                 break;
+             case PASSCODEERR:
+                 passcodeErrorState();
+                 break;
+             case ACCESSGRANTED:
+                 accessGrantedState();
+                 break;
+         }
+    }
+    return 0;
+}
 
+void idleState() {
+    lq_clear(&device);
+    lq_print(&device, "Scan Keycard");
 
-    PORTD.DIRSET = PIN1_bm;
-    
     Uid card;
 
-    while (1) {
+    while (currentState == IDLE) { // Stay in IDLE until a card is scanned
         if (readRfidCard(&card)) {
-            break;
+            // Format the UID into a string
+            char uidString[RFID_READER_BUFFER_SIZE] = {0};
+            for (uint8_t i = 0; i < card.size; i++) {
+                sprintf(&uidString[i * 2], "%02X", card.uidByte[i]);
+            }
+            // Send the UID to the controller via Bluetooth
+            bluetoothWriteBytes(uidString, strlen(uidString));
+            // Wait for controller response
+            char response[BUF_SIZE];
+            bluetoothReadBytes(response, 4);
+            if (strcmp(response, "AOK") == 0) {
+                currentState = PASSCODE;
+            } else {
+                currentState = RFIDERR;
+            }
         }
-        
-        PORTD.OUTTGL = PIN1_bm;
-        
-        _delay_ms(250);
     }
+}
 
-    return 0;
+void rfidErrorState() {
+    lq_clear(&device);
+    lq_print(&device, "Keycard Not Recognized");
+    _delay_ms(1000);
+    // Transition to IDLE
+    currentState = IDLE;
+}
+
+void passcodeState() {
+    lq_clear(&device);
+    lq_print(&device, "Enter Passcode");
+}
+
+void passcodeErrorState() {
+    lq_clear(&device);
+    lq_print(&device, "Incorrect Passcode");
+    _delay_ms(1000);
+    currentState = PASSCODE;
+}
+
+void accessGrantedState() {
+    lq_clear(&device);
+    lq_print(&device, "Lock Opened!");
+    _delay_ms(1000);
+    currentState = IDLE;
 }
